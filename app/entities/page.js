f5073add7472a14050dc25e480a4c2e68ab95837
jtgeme-gif <jtgeme@gmail.com>
2026-07-02 21:43:38 -0400

    rename edit button on entities

--- app/entities/page.js
+++ app/entities/page.js
@@ -94,7 +94,7 @@ export default function EntitiesPage() {
                     className="btn-small"
                     onClick={(ev) => { ev.stopPropagation(); setModalEntityId(e.id); }}
                   >
-                    Edit
+                    View
                   </button>
                 </div>
                 {isOpen && (
